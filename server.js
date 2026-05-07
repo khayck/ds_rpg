84136163501dc0de48806de04f34953045111f6e
khay <khayck.fronza@escola.pr.gov.br>
2026-05-07 11:41:47 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-app.post('/campo-batalha', (req, res) =>{
-    let nomeHeroi
-    let vidaHeroi 
-    let ataqueHeroi
-    let nomeMonstro
-    let vidaMonstro
-    let ataqueMonstro 
-})
+
+let nomeHeroi
+let vidaHeroi
+let ataqueHeroi
+let nomeMonstro
+let vidaMonstro
+let ataqueMonstro
+
+function realizarAtaque(ataqueHeroi, vidaMonstro){
+    danoMonstro = ataqueHeroi - vidaMonstro
+
+    if (vidaMonstro >= 0 ){
+       return console.log("Win")
+    } 
+}
+function re
