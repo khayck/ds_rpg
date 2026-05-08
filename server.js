3273b7c8ba38b786169e455ae03e67513f59a611
khay <khayck.fronza@escola.pr.gov.br>
2026-05-08 11:41:28 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
-app.post('/campo-batalha', (req, res) =>{
-    let nomeHeroi
-    let vidaHeroi 
-    let ataqueHeroi
-    let nomeMonstro
-    let vidaMonstro
-    let ataqueMonstro 
-})
+while (vidaHeroi > 0 && vidaMonstro > 0) {
+
+    function realizarAtaque(dano, vida) {
+        return vida - dano
+    }
+    function ataqueMonstro(dano, vida) {
+        return vida - dano
+    }
+
+    function iniciarTurno(vidaHeroi, vidaMonstro, ataqueHeroi, danoMonstro) {
+        nomeHeroi = "abacaxi"
+        vidaHeroi = 100
+        ataqueHeroi = 30
+        nomeMonstro = "gilgamaesh"
+        vidaMonstro = 300
+        danoMonstro = 20
+        danoCrit = Math.random()
+
+        realizarAtaque(ataqueHeroi, vidaMonstro)
+
+        ataqueMonstro(danoMonstro, vidaHeroi)
+
+        console.log(".......")
+        if (vidaHeroi <= 0){
+            console.log("vitory !!!!!")
+        }
+    }
+}
